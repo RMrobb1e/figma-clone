85b897e7142d9ffb20bc795514087c0349dc3258
robbie <robbiemariano126@gmail.com>
2024-02-27 21:26:09 +0800

    feat: add image functionality

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ import { ActiveElement } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
+import { handleImageUpload } from "@/lib/shapes";
 
 export const Page = () => {
   const undo = useUndo();
@@ -28,6 +29,7 @@ export const Page = () => {
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
+  const imageInputRef = useRef<HTMLInputElement | null>(null);
   const canvasObjects = useStorage((root) => root.canvasObjects);
   const syncShapeInStorage = useMutation(({ storage }, object) => {
     if (!object) return;
@@ -78,12 +80,22 @@ export const Page = () => {
         deleteAllShapes();
         fabricRef.current?.clear();
         setActiveElement(defaultNavElement);
-
         break;
 
       case "delete":
         handleDelete(fabricRef.current as any, deleteShapeFromStorage);
         setActiveElement(defaultNavElement);
+        break;
+
+      case "image":
+        imageInputRef.current?.click();
+        isDrawing.current = false;
+
+        if (fabricRef.current) {
+          fabricRef.current.isDrawingMode = false;
+        }
+
+        break;
 
       default:
         break;
@@ -162,6 +174,17 @@ export const Page = () => {
       <Navbar
         activeElement={activeElement}
         handleActiveElement={handleActiveElement}
+        imageInputRef={imageInputRef}
+        handleImageUpload={(e: any) => {
+          e.stopPropagation();
+
+          handleImageUpload({
+            file: e.target.files[0],
+            canvas: fabricRef as any,
+            shapeRef,
+            syncShapeInStorage
+          });
+        }}
       />
       <section className="flex h-full flex-row">
         <LeftSidebar allShapes={Array.from(canvasObjects)} />
