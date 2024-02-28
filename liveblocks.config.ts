73dfb4566294341a26789471bf28ba707f56e987
robbie <robbiemariano126@gmail.com>
2024-02-28 21:28:33 +0800

    feat: downgrade liveblocks

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -3,47 +3,7 @@ import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
 
 const client = createClient({
   throttle: 16,
-  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY!,
-  async resolveUsers({ userIds }) {
-    // Used only for Comments and Notifications. Return a list of user information
-    // retrieved from `userIds`. This info is used in comments, mentions etc.
-
-    // const usersData = await __fetchUsersFromDB__(userIds);
-    //
-    // return usersData.map((userData) => ({
-    //   name: userData.name,
-    //   avatar: userData.avatar.src,
-    // }));
-
-    return [];
-  },
-  async resolveMentionSuggestions({ text }) {
-    // Used only for Comments. Return a list of userIds that match `text`.
-    // These userIds are used to create a mention list when typing in the
-    // composer.
-    //
-    // For example when you type "@jo", `text` will be `"jo"`, and
-    // you should to return an array with John and Joanna's userIds:
-    // ["john@example.com", "joanna@example.com"]
-
-    // const users = await getUsers({ search: text });
-    // return users.map((user) => user.id);
-
-    return [];
-  },
-  async resolveRoomsInfo({ roomIds }) {
-    // Used only for Comments and Notifications. Return a list of room information
-    // retrieved from `roomIds`.
-
-    // const roomsData = await __fetchRoomsFromDB__(roomIds);
-    //
-    // return roomsData.map((roomData) => ({
-    //   name: roomData.name,
-    //   url: roomData.url,
-    // }));
-
-    return [];
-  }
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY!
 });
 
 // Presence represents the properties that exist on every user in the Room
@@ -134,7 +94,36 @@ export const {
     // useRoomInfo
   }
 } = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(
-  client
+  client,
+  {
+    async resolveUsers({ userIds }) {
+      // Used only for Comments and Notifications. Return a list of user information
+      // retrieved from `userIds`. This info is used in comments, mentions etc.
+
+      // const usersData = await __fetchUsersFromDB__(userIds);
+      //
+      // return usersData.map((userData) => ({
+      //   name: userData.name,
+      //   avatar: userData.avatar.src,
+      // }));
+
+      return [];
+    },
+    async resolveMentionSuggestions({ text }) {
+      // Used only for Comments. Return a list of userIds that match `text`.
+      // These userIds are used to create a mention list when typing in the
+      // composer.
+      //
+      // For example when you type "@jo", `text` will be `"jo"`, and
+      // you should to return an array with John and Joanna's userIds:
+      // ["john@example.com", "joanna@example.com"]
+
+      // const users = await getUsers({ search: text });
+      // return users.map((user) => user.id);
+
+      return [];
+    }
+  }
 );
 
 // Project-level hooks, use inside `LiveblocksProvider`
